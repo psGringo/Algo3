045317a516eb50442711fc9326748cf725bbe3c2
psGringo <panteleevstas@gmail.com>
2018-07-21 14:13:39 +0500

    123

---
+++ greatestCommonDivisor/greatestCommonDivisor/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace greatestCommonDivisor
+{
+    class Program
+    {
+        public static int Gcd(int p, int q)
+        {
+            if (q == 0) return p;
+            int r = p % q;
+            return Gcd(q, r);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Gcd(100, 2));
+            Console.ReadLine();
+        }
+    }
+}
