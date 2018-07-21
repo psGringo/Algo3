d6e9d516297bfd541f445bdfa7a2dd60d3c51b6b
psGringo <panteleevstas@gmail.com>
2018-07-21 14:57:49 +0500

    fixed stack

---
+++ 02_FixedStack/02_FixedStack/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_FixedStack
+{
+    class FixedStack<T>
+    {
+        private T[] Items; 
+        public int Count { get;set}
+        public FixedStack(int aCount)
+        {
+            Items = new T[aCount];            
+        }
+        public bool IsEmpty() { return Count == 0; }
+        public void Push(T item)
+        {
+            Items[Count++] = item;
+        }
+        public T Pop()
+        {
+            return Items[--Count];
+        }
+    }
+}
---
+++ 02_FixedStack/02_FixedStack/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_FixedStack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FixedStack<int> f = new FixedStack<int>(4);
+            f.Push(1);
+            f.Push(2);
+            f.Push(3);
+            //
+            Console.WriteLine("Count is..." + f.Count);
+            while (f.Count != 0)
+            {
+                Console.WriteLine(f.Pop().ToString());
+            }
+            Console.WriteLine("Count is..." + f.Count);
+            Console.ReadLine();
+        }
+    }
+}
