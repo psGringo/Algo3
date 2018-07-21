0337552b0dbb36a4f594f1097019f7c21a51f9e2
psGringo <panteleevstas@gmail.com>
2018-07-21 15:58:22 +0500

    stack on linked list

---
+++ 03_StackOnLinkedList/03_StackOnLinkedList/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_StackOnLinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StackOnLinkedList<int> s = new StackOnLinkedList<int>();
+            s.Push(1);
+            s.Push(2);
+            s.Push(3);
+            // enumeration
+            //foreach (var el in s) Console.WriteLine(el);
+            
+
+            // Pop
+            while (s.Count != 0)
+            {
+                Console.WriteLine(s.Pop().ToString());
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 03_StackOnLinkedList/03_StackOnLinkedList/StackOnLinkedList.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_StackOnLinkedList
+{
+    class StackOnLinkedList<T> : IEnumerable<T>
+    {
+        class Node<T>
+        {
+            public T item;
+            public Node<T> next;
+        }
+        Node<T> first;
+        public int Count { get; set; }
+        public bool IsEmpty() { return Count == 0; }
+        public void Push(T item)
+        {
+            Node<T> tempNode = first;
+            first = new Node<T>();
+            first.item = item;
+            first.next = tempNode;
+            Count++;
+        }
+
+        public T Pop()
+        {
+            T item = first.item;
+            first = first.next;
+            Count--;
+            return item;
+        }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            var node = first;
+            while (node != null)
+            {
+                yield return node.item;
+                node = node.next;
+            }
+        }
+    }
+}
